3fc00630591829ddbdffa752308973857e82d543
Grant David Meadors <grant.meadors@ligo.org>
2016-04-09 20:44:01 +0200

    Can now do a Fourier transform on a sine wave. So let's see what happens when we stack the sine wave on top of itself.

--- finiteDiff.py
+++ finiteDiff.py
@@ -1,4 +1,4 @@
-#!/opt/local/bin/python
+#!/usr/bin/env python
 # Grant David Meadors
 # 02016-04-09 (JD 2457488)
 # g r a n t . m e a d o r s @ a e i . m p g . d e
@@ -18,22 +18,40 @@ rc('text',usetex=True)
 Fsample = 16384.0
 Tobs = 2.0
 
+# Noise
+noiseAmp = 1.0
+noise = noiseAmp * np.random.randn(Fsample*Tobs)
+
+# Signal
+signalAmp = 100.0
+signalFreq = 200.0
+time = np.linspace(0, Tobs, Fsample*Tobs)
+signal = signalAmp * np.sin(2*np.pi*signalFreq*time) 
+
+# Observed
+observed = signal + noise
+
+print observed
 
 # Frequency bins of the FFT
 fftBins = np.linspace(0, Fsample, Fsample*Tobs)
 
+print fftBins
+
 # FFT the data
-H1fft = np.fft.fft(H1dataZeroed[:,1])
-H1asd = np.absolute(H1fft)
+fftObserved = np.fft.fft(observed)
+asdObserved = np.absolute(fftObserved)
+
+print asdObserved
 
 # Plot the data
 def plotASD(xBins, asdData, fileName):
     fig = plt.figure()
-    plt.plot(fftBins,H1asd)
-    plt.axis([100,300,0,2e-17])
+    plt.semilogy(fftBins,asdObserved)
+    plt.axis([0,500,1e-2,1e10])
     plt.xlabel('Frequency [Hz]')
     plt.title('Amplitude spectral density')
     fig.savefig(fileName)
     plt.close()
 
-plotASD(fftBins, H1asd, 'H1asd.png')
+plotASD(fftBins, asdObserved, 'asdObserved.png')
